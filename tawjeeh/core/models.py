31d766554234a4f982f5fe74dc3eb891a192f2b0
MagedSaeed <mageedsaeed1@gmail.com>
2024-05-02 10:45:26 +0300

    settings and managements

--- tawjeeh/core/models.py
+++ tawjeeh/core/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+
+# accounts/models.py
+
+class TawjeehUser(AbstractUser):
+
+    # Override any methods from AbstractUser if needed
+    def __str__(self):
+        return self.username
