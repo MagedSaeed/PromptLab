8e30907690c646a8ac5028a5afb329b1e5235726
MagedSaeed <mageedsaeed1@gmail.com>
2024-05-02 09:18:12 +0300

    make sure static are collected

--- docker/django-site/server-entrypoint.sh
+++ docker/django-site/server-entrypoint.sh
@@ -6,16 +6,14 @@ do
 done
 
 
-until python3 manage.py migrate
+until python manage.py migrate
 do
     echo "Waiting for db to be ready..."
     sleep 2
 done
 
 
-python3 manage.py collectstatic --noinput
-
-# python manage.py createsuperuser --noinput
+python manage.py collectstatic --noinput
 
 gunicorn tawjeeh.wsgi --bind 0.0.0.0:8000 --workers 4 --threads 4
 
