bf0508abcd697266d876dcb4ff0be76e9d6782b2
MagedSaeed <mageedsaeed1@gmail.com>
2024-05-15 17:30:57 +0300

    add datasets list page

--- tawjeeh/prompt/urls.py
+++ tawjeeh/prompt/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from prompt.views import TaskListView
+from prompt.views import TaskListView, DatasetListView
 
 app_name = "prompt"
 
 urlpatterns = [
     path("task/list", TaskListView.as_view(), name="task_list"),
+    path("dataset/list", DatasetListView.as_view(), name="dataset_list"),
 ]
--- tawjeeh/prompt/views.py
+++ tawjeeh/prompt/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 
-from prompt.models import Task
-from prompt.filters import TaskFilter
+from prompt.models import Task, Dataset
 from django.views.generic import ListView
 from django_filters.views import FilterView
+from prompt.filters import TaskFilter, DatasetFilter
 
 
 class TaskListView(FilterView, ListView):
@@ -34,4 +34,46 @@ class TaskListView(FilterView, ListView):
     def render_to_response(self, context, **response_kwargs):
         if self.request.htmx:
             return render(self.request, "prompt/partials/task_list_table.html", context)
+        return super().render_to_response(context, **response_kwargs)
+
+
+class DatasetListView(FilterView, ListView):
+    model = Dataset
+    paginate_by = 6
+    ordering = "name"
+    filterset_class = DatasetFilter
+    context_object_name = "datasets"
+    template_name = "prompt/dataset_list.html"
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        task_pk = self.request.GET.get("task_pk")
+        self.task = None
+        if task_pk:
+            queryset = queryset.filter(task__pk=task_pk)
+            self.task = Task.objects.get(pk=task_pk)
+        filterset = self.filterset_class(self.request.GET, queryset=queryset)
+        filtered_qs = filterset.qs
+        search_term = self.request.GET.get("name", "")
+        if search_term:
+            terms = list(filter(None, search_term.split()))
+            for term in terms:
+                filtered_qs |= queryset.filter(name__icontains=term)
+        return filtered_qs
+
+    def get_context_data(self, **kwargs):
+        filtered_datasets = self.get_queryset()
+        kwargs["object_list"] = filtered_datasets
+        context = super().get_context_data(**kwargs)
+        context["task"] = self.task
+        return context
+
+    def render_to_response(self, context, **response_kwargs):
+        if self.request.htmx:
+            print(self.request)
+            return render(
+                self.request,
+                "prompt/partials/dataset_list_table.html",
+                context,
+            )
         return super().render_to_response(context, **response_kwargs)
