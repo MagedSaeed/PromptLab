f729876e2ace093a980679c6028700f4636af2e2
MagedSaeed <mageedsaeed1@gmail.com>
2024-05-15 17:30:57 +0300

    add datasets list page

--- tawjeeh/prompt/filters.py
+++ tawjeeh/prompt/filters.py
@@ -1,5 +1,5 @@
 import django_filters
-from prompt.models import Task
+from prompt.models import Task, Dataset
 
 
 class TaskFilter(django_filters.FilterSet):
@@ -7,4 +7,12 @@ class TaskFilter(django_filters.FilterSet):
 
     class Meta:
         model = Task
+        fields = ["name"]
+
+
+class DatasetFilter(django_filters.FilterSet):
+    name = django_filters.CharFilter(field_name="name", lookup_expr="icontains")
+
+    class Meta:
+        model = Dataset
         fields = ["name"]
