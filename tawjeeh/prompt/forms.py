ffb053f721b5f458619eda985ccc2456a439c105
MagedSaeed <mageedsaeed1@gmail.com>
2024-05-29 16:56:47 +0300

    update admin forms with config choices, although it is not working yet for some reason till now.

--- tawjeeh/prompt/forms.py
+++ tawjeeh/prompt/forms.py
@@ -1,5 +1,19 @@
 from django import forms
-from prompt.models import Prompt
+from prompt.models import Dataset, Prompt
+from prompt.utils import get_hf_dataset_config_choices
+
+
+class DatasetAdminForm(forms.ModelForm):
+    class Meta:
+        model = Dataset
+        fields = "__all__"
+
+    def __init__(self, *args, **kwargs):
+        super(DatasetAdminForm, self).__init__(*args, **kwargs)
+        if self.instance and self.instance.pk:
+            self.fields["config"].choices = get_hf_dataset_config_choices(
+                self.instance.huggingface_name
+            )
 
 
 class PromptCreateForm(forms.ModelForm):
