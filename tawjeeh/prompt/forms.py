78dc579caf52c843f356b33388f85cae524f98f2
Maged Saeed <mageedsaeed1@gmail.com>
2024-06-10 09:32:33 +0300

    save the subset along with the promt

--- tawjeeh/prompt/forms.py
+++ tawjeeh/prompt/forms.py
@@ -1,3 +1,4 @@
+import datasets
 from django import forms
 from prompt.models import Prompt
 
@@ -5,9 +6,15 @@ from prompt.models import Prompt
 class PromptCreateForm(forms.ModelForm):
     class Meta:
         model = Prompt
-        fields = ["name", "template", "answer_choices", "dataset_subset"]
+        fields = [
+            "name",
+            "template",
+            "answer_choices",
+            "dataset_subset",
+        ]
 
     def __init__(self, *args, **kwargs):
+        self.dataset = kwargs.pop("dataset")
         super().__init__(*args, **kwargs)
         self.fields["answer_choices"].widget.attrs.update(
             {
@@ -19,7 +26,17 @@ class PromptCreateForm(forms.ModelForm):
         )
         # make dataset_subset hidden as this will be handled by the ui from the dataset information left sidebar
         self.fields["dataset_subset"].widget = forms.HiddenInput()
-        self.fields["dataset_subset"].required = True
-        self.fields["dataset_subset"].error_messages = {
-            "required": "Please select a dataset from the left sidebar first.",
-        }
+        self.instance.dataset = self.dataset
+        if len(self.instance.dataset.subsets_with_splits) > 1:
+            self.fields["dataset_subset"].required = True
+            self.fields["dataset_subset"].error_messages = {
+                "required": "Please select a dataset from the left sidebar first.",
+            }
+
+    def save(self, commit=True):
+        if not len(self.instance.dataset.subsets_with_splits) > 1:
+            self.instance.dataset_subset = datasets.get_dataset_default_config_name(
+                self.dataset.huggingface_name,
+                trust_remote_code=True,
+            )
+        return super().save(commit=commit)
