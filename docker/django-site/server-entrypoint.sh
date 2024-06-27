44c52fabea2c86e2f46ddd8a8ed7d93fd09f3898
MagedSaeed <mageedsaeed1@gmail.com>
2024-06-27 13:28:59 +0300

    fix a bug in deployment

--- docker/django-site/server-entrypoint.sh
+++ docker/django-site/server-entrypoint.sh
@@ -41,8 +41,6 @@ done
 # collect static
 python3 manage.py collectstatic --noinput
 
-# get datasets from huggingface
-python3 manage.py sync_with_hf
 
 DJANGO_SETTINGS_MODULE=tawjeeh.production_settings
 
