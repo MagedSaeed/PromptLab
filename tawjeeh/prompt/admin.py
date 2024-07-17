e4f5451dcf0667e7a3a64f4355c4e74c313ff9ff
MagedSaeed <mageedsaeed1@gmail.com>
2024-07-17 13:54:17 +0300

    disable hf syncing on admin as it is not much useful with the current setup

--- tawjeeh/prompt/admin.py
+++ tawjeeh/prompt/admin.py
@@ -1,27 +1,39 @@
-from django.contrib import admin, messages
-from django.core.management import call_command
+from django.contrib import admin
+
+# from django.core.management import call_command
 from prompt.models import Dataset, Prompt, PromptReviewAction, Task
+
+# from django.contrib import messages
+
 
 # Register your models here.
 
 
-@admin.action(description="Sync with Hugging Face")
-def sync_with_huggingface(modeladmin, request, queryset):
-    try:
-        call_command("sync_with_hf")
-        modeladmin.message_user(
-            request, "Successfully synced with Hugging Face", messages.SUCCESS
-        )
-    except Exception as e:
-        modeladmin.message_user(
-            request, f"Error syncing with Hugging Face: {e}", messages.ERROR
-        )
+# @admin.action(description="Sync with Hugging Face")
+# def sync_with_huggingface(modeladmin, request, queryset):
+#     try:
+#         call_command(
+#             "sync_with_hf",
+#             sheet_url="1kIDS-fwO5l6sH2ZBDCepOJeNyOh2j7Wb-w3W0JChi2k",
+#             sheet_name="final-list",
+#         )
+#         modeladmin.message_user(
+#             request,
+#             "Successfully synced with Hugging Face",
+#             messages.SUCCESS,
+#         )
+#     except Exception as e:
+#         modeladmin.message_user(
+#             request,
+#             f"Error syncing with Hugging Face: {e}",
+#             messages.ERROR,
+#         )
 
 
 class TaskAdmin(admin.ModelAdmin):
     search_fields = ["name"]
     list_filter = ["name"]
-    actions = [sync_with_huggingface]
+    # actions = [sync_with_huggingface]
 
 
 class DatasetAdmin(admin.ModelAdmin):
