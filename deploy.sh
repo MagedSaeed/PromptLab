f78923da8ca7e2df5629721ec8a5951cae2b755c
MagedSaeed <mageedsaeed1@gmail.com>
2024-07-23 08:48:03 +0300

    explicity bind gunicorn to 8080

--- deploy.sh
+++ deploy.sh
@@ -46,4 +46,4 @@ celery -A tawjeeh worker -l info &
 celery -A tawjeeh beat -l info --scheduler django_celery_beat.schedulers:DatabaseScheduler &
 
 # Start the Gunicorn server in the background
-gunicorn tawjeeh.wsgi --workers 4 --threads 4
+gunicorn tawjeeh.wsgi --workers 4 --threads 4 --bind 0.0.0.0:8080
