036256953b2e3ac2aead1516b4e9fda65a7fbebb
MagedSaeed <mageedsaeed1@gmail.com>
2024-07-30 11:07:12 +0300

    create a tawjeeh user and give it the project permission

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # Create and activate a virtual environment
 python3 -m venv venv
 source venv/bin/activate
@@ -8,22 +10,22 @@ pip install -r requirements.txt
 # Navigate to the project directory
 cd tawjeeh
 
-# set production settings as env variable
+# Set production settings as env variable
 export DJANGO_SETTINGS_MODULE=tawjeeh.production_settings
 
-# migrate django sites first
+# Migrate Django sites first
 python manage.py migrate sites
 
 # Run Django management commands
 python manage.py migrate
 
-# create superusers
+# Create superusers
 python manage.py import_superusers ../docker/django-site/admins.yml
 
-# setup allauth
+# Setup allauth
 python manage.py setup_allauth
 
-# sync with hf
+# Sync with HF
 python manage.py sync_with_hf \
     --sheet_id 1kIDS-fwO5l6sH2ZBDCepOJeNyOh2j7Wb-w3W0JChi2k \
     --sheet_name final-list \
@@ -33,17 +35,23 @@ python manage.py sync_with_hf \
     --answer_choices_column answer_choices \
     --clear_datasets False
 
-# collect static
+# Collect static
 python manage.py collectstatic --noinput
 
-# install gunicorn
+# Install Gunicorn
 pip install gunicorn
 
-# run celery worker
-celery -A tawjeeh worker -l info &
+# Create a non-root user
+useradd -m -d /home/tawjeeh tawjeeh
 
-# run celery beat
-celery -A tawjeeh beat -l info --scheduler django_celery_beat.schedulers:DatabaseScheduler &
+# Set permissions for the virtual environment and project directory
+chown -R tawjeeh:tawjeeh /app/venv /app/tawjeeh
+
+# Run Celery worker as the non-root user
+sudo -u tawjeeh -E bash -c "source /app/venv/bin/activate && celery -A tawjeeh worker -l info &"
+
+# Run Celery beat as the non-root user
+sudo -u tawjeeh -E bash -c "source /app/venv/bin/activate && celery -A tawjeeh beat -l info --scheduler django_celery_beat.schedulers:DatabaseScheduler &"
 
 # Start the Gunicorn server in the background
 gunicorn tawjeeh.wsgi --workers 4 --threads 4 --bind 0.0.0.0:8080
