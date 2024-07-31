afd70fae08ba9647c919f6bf69eb5166aceda3b4
MagedSaeed <mageedsaeed1@gmail.com>
2024-07-31 13:57:02 +0300

    update celery settings

--- tawjeeh/tawjeeh/celery.py
+++ tawjeeh/tawjeeh/celery.py
@@ -22,9 +22,9 @@ app.conf.enable_utc = True  # Ensure UTC is enabled for proper timezone conversi
 app.autodiscover_tasks()
 
 app.conf.update(
-    worker_max_memory_per_child=1048576 // 4,  # 0.25 GB in kilobytes
-    task_time_limit=60 * 5,  # 5 minutes
-    task_soft_time_limit=60 * 10,  # 10 minutes
+    worker_max_memory_per_child=1048576 // 20,  # 0.05 GB in kilobytes
+    task_time_limit=60 * 1,  # 5 minutes
+    task_soft_time_limit=60 * 2,  # 10 minutes
     worker_max_tasks_per_child=100,
     worker_prefetch_multiplier=1,
     worker_concurrency=1,  # set only one worker
