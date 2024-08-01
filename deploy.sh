106cb69a2d5870a42cbfd85386fdc0f4b9e3eba6
MagedSaeed <mageedsaeed1@gmail.com>
2024-08-01 12:53:08 +0300

    update some settings to enhance memory usage vs tasks performance

--- deploy.sh
+++ deploy.sh
@@ -48,4 +48,4 @@ pip install gunicorn
 celery -A tawjeeh beat -l info --scheduler django_celery_beat.schedulers:DatabaseScheduler &
 
 # Start the Gunicorn server in the background
-gunicorn tawjeeh.wsgi --workers 4 --threads 4 --bind 0.0.0.0:8080
+gunicorn tawjeeh.wsgi --workers 2 --threads 4 --bind 0.0.0.0:8080
