f08b6cf3fde06234bf43cb487d6a9c2550a6adef
MagedSaeed <mageedsaeed1@gmail.com>
2024-08-01 12:53:08 +0300

    update some settings to enhance memory usage vs tasks performance

--- tawjeeh/tawjeeh/celery.py
+++ tawjeeh/tawjeeh/celery.py
@@ -24,10 +24,10 @@ app.conf.enable_utc = True  # Ensure UTC is enabled for proper timezone conversi
 app.autodiscover_tasks()
 
 app.conf.update(
-    worker_max_memory_per_child=1048576 // 50,  # 0.02 GB in kilobytes
-    task_time_limit=60 * 1,  # 1 minutes
-    task_soft_time_limit=60 * 2,  # 2 minutes
+    worker_max_memory_per_child=1048576 // 10,  # 0.1 GB in kilobytes
+    task_time_limit=60 * 3,  # 3 minutes
+    task_soft_time_limit=60 * 5,  # 5 minutes
     worker_max_tasks_per_child=5,
-    worker_prefetch_multiplier=1,
-    worker_concurrency=1,  # set only one worker
+    worker_prefetch_multiplier=2,
+    worker_concurrency=2,  # set two concurrent workers
 )
