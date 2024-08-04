db97765ab72adc8ec6386b2728825feb55057ce0
MagedSaeed <mageedsaeed1@gmail.com>
2024-08-04 10:31:30 +0300

    update celery worker settings

--- tawjeeh/tawjeeh/celery.py
+++ tawjeeh/tawjeeh/celery.py
@@ -27,7 +27,7 @@ app.conf.update(
     worker_max_memory_per_child=1048576 // 20,  # 0.05 GB in kilobytes
     task_time_limit=60 * 3,  # 3 minutes
     task_soft_time_limit=60 * 5,  # 5 minutes
-    worker_max_tasks_per_child=5,
-    worker_prefetch_multiplier=2,
+    worker_max_tasks_per_child=3,
+    worker_prefetch_multiplier=1,
     worker_concurrency=1,  # set one concurrent worker(s)
 )
