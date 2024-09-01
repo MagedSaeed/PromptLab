a73037f8b385e994f2065620d15a8fe7e78990ac
MagedSaeed <mageedsaeed1@gmail.com>
2024-09-01 15:27:03 +0300

    add default subset to the deploy script

--- deploy.sh
+++ deploy.sh
@@ -33,7 +33,8 @@ python manage.py sync_with_hf \
     --answer_choices_column answer_choices \
     --is_single_classification_column is_single_classification \
     --target_column target_column \
-    --clear_datasets False
+    --clear_datasets False \
+    --default_subset subset
 
 # collect static
 python manage.py collectstatic --noinput
