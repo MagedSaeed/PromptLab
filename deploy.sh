890f2b257d747d31f641afa849f2ea7cad121bd6
Maged Saeed <mageedsaeed1@gmail.com>
2025-06-17 20:10:32 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -54,4 +54,5 @@ pip install gunicorn
 celery -A tawjeeh beat -l info --scheduler django_celery_beat.schedulers:DatabaseScheduler &
 
 # Start the Gunicorn server in the background
-gunicorn tawjeeh.wsgi --workers 4 --thread 4 --bind 0.0.0.0:8080 --max-requests 20 --max-requests-jitter 10 --preload --timeout 150
+#gunicorn tawjeeh.wsgi --workers 4 --thread 4 --bind 0.0.0.0:8080 --max-requests 20 --max-requests-jitter 10 --preload --timeout 150
+gunicorn tawjeeh.wsgi --workers 4 --thread 4 --bind 0.0.0.0:8080
