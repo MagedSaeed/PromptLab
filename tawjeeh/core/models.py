a72230d61c664fd19028b6ca6d180bfa13a86a38
MagedSaeed <mageedsaeed1@gmail.com>
2025-12-03 15:41:28 +0300

    Fix is_moderator property to return is_staff instead of is_superuser

--- tawjeeh/core/models.py
+++ tawjeeh/core/models.py
@@ -4,15 +4,15 @@ from django.db import models
 
 class TawjeehUser(AbstractUser):
     openrouter_api_key = models.CharField(max_length=255, blank=True, null=True)
-    
+
     # Override any methods from AbstractUser if needed
     def __str__(self):
         return self.username
 
     @property
     def is_moderator(self):
-        return self.is_superuser
-        
+        return self.is_staff
+
     @property
     def masked_openrouter_api_key(self):
         """Return a masked version of the API key for display"""
